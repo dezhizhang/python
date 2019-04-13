1eca46d684a5a6c1b8c50a61c98e37140fc6ce11
dezhizhang <1541609448@qq.com>
2019-04-13 22:06:57 +0800

    进制转换

--- index.py
+++ index.py
@@ -3,6 +3,7 @@
 # print(0b11010);
 # print(0b01);
 # print(0b01);
+
 #八进制
 # print(0o2);
 # print(0o21);
@@ -15,6 +16,41 @@
 # print(0x10);
 # print(0x10);
 # print(0x10);
+# print(type(10));
+
+#十进制转二进制
+# print(bin(10));
+# print(bin(10));
+# print(bin(10));
+# print(bin(10));
+
+#任意数转为十进制
+# print(int(12.12));
+# print(int(0b10));
+# print(int(0b10));
+# print(int(0o12));
+
+#任意数字转为十六进制
+# print(hex(0b10));
+# print(hex(10));
+# print(hex(10));
+# print(hex(10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
