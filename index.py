5022100716ea478b64bf6a800ec529e1727ea389
dezhizhang <1541609448@qq.com>
2019-04-13 21:51:58 +0800

    十六进制

--- index.py
+++ index.py
@@ -3,8 +3,28 @@
 # print(0b11010);
 # print(0b01);
 # print(0b01);
+#八进制
 # print(0o2);
-print(0o21);
+# print(0o21);
+# print(0o2);
+# print(0o2);
+
+#十六进制
+# print(0x11);
+# print(0x10);
+# print(0x10);
+# print(0x10);
+# print(0x10);
+
+
+
+
+
+
+
+
+
+
 
 
 
