398ca7ec58998d95c7db00411ee9759ec5f3dfcf
dezhizhang <1541609448@qq.com>
2019-04-27 15:49:54 +0800

    枚举遍历

--- index.py
+++ index.py
@@ -22,6 +22,61 @@
 
 # print(VIP.red.name)
 
+# from enum import Enum
+# class VIP(Enum):
+#     red = 1
+#     yellow = 2
+
+# for item in VIP:
+#     print(item.name)
+
+# from enum import Enum
+# class VIP(Enum):
+#     red = 1
+#     yellow  = 2
+
+# for item in VIP:
+#     print(item.name)
+
+# from enum import Enum
+# class VIP(Enum):
+#     red = 1
+#     yellow = 2
+
+# for item in VIP:
+#     print(item.value)
+
+# from enum import Enum
+# class VIP(Enum):
+#     red = 1
+#     yellow = 2
+
+# for item in VIP:
+#     print(item.name)
+
+# from enum import Enum
+# class VIP(Enum):
+#     red = 1
+#     yellow = 2
+
+# for item in VIP:
+#     print(item.value)
+
+# from enum import Enum
+# class VIP(Enum):
+#     red = 1
+#     yellow = 2
+
+# result = VIP.red.value > VIP.yellow.value
+# print(result)
+
+
+
+
+
+
+
+
 
 
 
