0810ec08a040ce53a69c5252f31911add436788e
dezhizhang <1541609448@qq.com>
2019-04-27 18:44:11 +0800

    三元远算符

--- index.py
+++ index.py
@@ -2,8 +2,80 @@
 # f = lambda x,y:x+y
 # print(f(1,2))
 
-f = lambda x,y:x + y
-print(f(1,4))
+# f = lambda x,y:x + y
+# print(f(1,4))
+
+# f = lambda x,y:x + y
+# print(f(10,20))
+
+# f = lambda x,y:x + y
+# print(f(10,20))
+
+# f = lambda x,y:x + y
+# print(f(10,12))
+
+# f = lambda x,y:x +y
+# print(f(10,10))
+
+# f = lambda x,y:x + y
+# print(f(10,10))
+
+
+
+# f = lambda x,y: x ? y
+
+# x = 1
+# y = 3
+
+# r = x if x > y else y
+# print(r)
+# x = 1
+# y = 3
+# r = x if x > y else y
+# print(r)
+
+# x = 1
+# y = 3
+# r = x if x > y else y
+# print(x)
+
+# x = 1
+# y = 3
+# r = x if x > y else y
+# print(x)
+
+# x = 1
+# y = 3
+# r = x if x > y else y
+# print(r)
+
+# x = 1
+# y = 2
+
+# r = x if x > y else y
+# print(r)
+
+# x = 1
+# y = 2
+
+# r = x if x > y else y 
+# print(r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
