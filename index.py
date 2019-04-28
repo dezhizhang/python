7b8b562cb194f48f214976e1a2d1286f296dedfc
dezhizhang <1541609448@qq.com>
2019-04-28 18:24:38 +0800

    mongodb中添加数据

--- index.py
+++ index.py
@@ -1,13 +1,31 @@
-import pymongo
+# import pymongo
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# mydb = myclient["cms"]
+# mycol = mydb["hello"]
  
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["cms"]
-mycol = mydb["hello"]
+# mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
  
-mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
- 
-x = mycol.insert_one(mydict) 
-print(x)
+# x = mycol.insert_one(mydict) 
+# print(x)
+
+# import pymongo
+# myclient = pymongo.MongoClient('mongodb://localhost:27017')
+# mydb = myclient['cms']
+# mycol = mydb['zhangdezhi']
+# mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
+# x = mycol.insert_one(mydict)
+# print(x)
+
+# import pymongo
+# myclient = pymongo.MongoClient('mongodb://localhost:27017')
+# mydb = myclient['cms']
+# mycol = mydb['hello']
+# mydict =  { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
+# x = mycol.insert_one(mydict)
+# print(x)
+
+
+
 
 
 
