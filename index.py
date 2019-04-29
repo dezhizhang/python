7a1c0a2dec36214e3a93881c427e86eb141d3437
dezhizhang <1541609448@qq.com>
2019-04-29 22:29:03 +0800

    爬虫

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
 
-num = 123
-print(num)
+from urllib import request
+
+if __name__ == '__main__':
+    urls = 'http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201902031029.html'
+    res = request.urlopen(urls)
+    html = res.read()
+    html = html.decode()
+
+print(html)
+
+
