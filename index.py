a76b25a3264f0a2ea06e573533efdfb97a3b4152
dezhizhang <1541609448@qq.com>
2019-04-29 22:38:42 +0800

    编码问题

--- index.py
+++ index.py
@@ -1,12 +1,20 @@
 
-from urllib import request
+# from urllib import request
 
+# if __name__ == '__main__':
+#     urls = 'http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201902031029.html'
+#     res = request.urlopen(urls)
+#     html = res.read()
+#     html = html.decode()
+
+# print(html)
+
+from urllib import request
 if __name__ == '__main__':
-    urls = 'http://www.mca.gov.cn/article/sj/xzqh/2019/201901-06/201902031029.html'
+    urls = 'http://dag.mca.gov.cn/'
     res = request.urlopen(urls)
     html = res.read()
     html = html.decode()
-
 print(html)
 
 
