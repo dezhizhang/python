4b2eaf1a1cba9903116d3650515ebfc9b483e2ae
dezhizhang <1541609448@qq.com>
2019-05-01 16:36:26 +0800

    url重命名

--- leaning/index/views.py
+++ leaning/index/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect,reverse
 
 def index(request):
+    index_url = reverse('index')
+    print(index_url)
     return HttpResponse('前台首页')
 
 def login(request):
     return HttpResponse('前台登录页')
-    
+
