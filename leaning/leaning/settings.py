1c6a5cbe05880dfb30f4182fa20e02aa87ef0198
dezhizhang <1541609448@qq.com>
2019-05-02 19:03:23 +0800

    数据库连接

--- leaning/leaning/settings.py
+++ leaning/leaning/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'leaning.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cms_1',
+        'USER':'root',
+        'PASSWORD':'701XTAY1993',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
