b2c0d45f2ca15433b0ce04f37fe549d94508fa95
dezhizhang <1541609448@qq.com>
2019-05-02 13:31:11 +0800

    函数的调用

--- leaning/leaning/views.py
+++ leaning/leaning/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def greet():
+    return 'hello world'
+
 def index(request):
     context = {
-
+        'greet':greet
     }
     return render(request,'index.html',context = context)
 
