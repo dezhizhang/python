2a57196a422f6fbf8afb82e047548a9ca570a751
dezhizhang <1541609448@qq.com>
2019-05-03 19:22:00 +0800

    修改文件

--- leaning/leaning/urls.py
+++ leaning/leaning/urls.py
@@ -18,5 +18,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name = 'index'),
+    path('add_book/',views.add_book,name = 'add_book'),
+    path('book_detail',views.book_detail,name = 'book_detail')
 ]
     
--- leaning/leaning/views.py
+++ leaning/leaning/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connection
 
+
+def get_corsor():
+    return connection.corsor()
+
+
 def index(request):
     return render(request,'index.html')
+
+
+def add_book(request):
+    return render(request,'add_book.html')
+
+def book_detail(request,book_id):
+    return render(request,'book_detail.html')
+
 
 
 
