bc4332831fb4f85a79279adfe277298f6d35b2f1
dezhizhang <1541609448@qq.com>
2019-05-03 20:34:01 +0800

    数据添加成功

--- leaning/leaning/views.py
+++ leaning/leaning/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
 from django.db import connection
 
@@ -6,16 +6,21 @@ from django.db import connection
 def get_cursor():
     return connection.cursor()
 
-
 def index(request):
     cursor = get_cursor()
     cursor.execute('select id,name,author from book')
     books = cursor.fetchall()
     return render(request,'index.html',context={'books':books})
 
-
 def add_book(request):
-    return render(request,'add_book.html')
+    if request.method == 'GET':
+        return render(request,'add_book.html')
+    else:
+        name = request.POST.get('name')
+        author = request.POST.get('author')
+        cursor = get_cursor()
+        cursor.execute("insert into book(id,name,author) values(null,'%s','%s')" % (name,author))
+        return redirect(reverse('index'))
 
 def book_detail(request,book_id):
     return render(request,'book_detail.html')
