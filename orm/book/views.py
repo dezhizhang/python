20cd8a2da79655052fb343b72c7d31b438015e8f
dezhizhang <1541609448@qq.com>
2019-05-04 17:33:13 +0800

    数据的增删改查

--- orm/book/views.py
+++ orm/book/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
+def index(request):
+    book = Book(name='web安全',author='吴大人',price=100)
+    book.save()
+    return HttpResponse('图书添加成功')
+
+def search(request):
+    book = Book.objects.filter(name='三国志')
+    print(book)
+    return HttpResponse('查询图书成功')
+
+def update(request):
+    book = Book.objects.get(pk=5)
+    book.price = 200
+    book.save()
+    return HttpResponse('更新数据成功')
+
+def delete(request):
+    book = Book.objects.filter(name='三国志')
+    book.delete()
+    return HttpResponse('删除数据成功')
+
+
+
+
+
--- orm/orm/urls.py
+++ orm/orm/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from book import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index,name = 'index'),
+    path('search/',views.search,name = 'search'),
+    path('delete/',views.delete,name = 'delete'),
+    path('update/',views.update,name = 'update')
 ]
