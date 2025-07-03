cf8b43b38e70e498255350b53b85c3fa7fb54b4e
dezhizhang <1541609448@qq.com>
2025-07-04 05:19:30 +0800

    feat:update

--- lesson01/main.py
+++ lesson01/main.py
@@ -1,5 +1,10 @@
 
 
-print("hello world")
-print("info change")
+# print("hello world")
+# print("info change")
+
+print(666)
+print(13.14)
+print("黑马程序员")
+
 
---
+++ lesson02/main.py
@@ -0,0 +1,16 @@
+# money = 50
+# print(money)
+#
+# print("钱包还有:",money)
+
+# money = 50
+# print(money)
+#
+# print("钱包还有:",money)
+
+money = 50
+print("钱包还有:",money)
+
+money = money - 10
+print("买了哈哈花费了10元还",40,"元")
+
