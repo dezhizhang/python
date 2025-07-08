74060c01f802daaf763d0dd5ebce79daf8c2d2ab
dezhizhang <1541609448@qq.com>
2025-07-09 07:36:40 +0800

    feat: 字符串格式化

---
+++ lesson13/main.py
@@ -0,0 +1,18 @@
+# name = 'tom'
+#
+# set_up_year = 2006
+# stock_price = 19.99
+#
+# print(f"我是{name}成立于:{set_up_year},股票价:{stock_price}")
+
+# name = 'tom'
+# set_up_year = 2006
+# stock_price = 19.99
+#
+# print(f"我是{name} 成立于:{set_up_year} 股票价:{stock_price}")
+
+name = 'tom'
+set_up_year = 2006
+stock_price = 19.99
+
+print(f"我是:{name} 成立于：{set_up_year} 股票价:{stock_price}")
