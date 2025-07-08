159864bc85596944239098c15f8e1ed37cc642ac
dezhizhang <1541609448@qq.com>
2025-07-09 07:19:33 +0800

    feat: 精度控制

---
+++ lesson11/main.py
@@ -0,0 +1,16 @@
+
+
+# name = "tom"
+# setup_year = 2006
+# stock_price = 19.99
+#
+#
+# message = "%s 成立于:%d,我今天的股票价格:%f" % (name,setup_year,stock_price)
+# print(message)
+
+name = "tom"
+setup_year = 2006
+stock_price = 19.99
+
+message = "%s 成立于:%d, 我今天的股票价格:%f" % (name,setup_year,stock_price)
+print(message)
