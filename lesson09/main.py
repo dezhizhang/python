7639535fc83217c9392e02edc7f19dce25317252
dezhizhang <1541609448@qq.com>
2025-07-09 07:07:37 +0800

    feat: 字符串拼接

---
+++ lesson09/main.py
@@ -0,0 +1,20 @@
+# name = "tom"
+# address = "北景路"
+#
+# print("我是:" + name + "我的地址是:" + address)
+#
+
+# name = 'tom'
+# tel = 4006199090
+# print("我是:"+name + "电话:" + str(tel))
+
+# name = "tom"
+# tel = 15992478448
+# message = "姓名%s 电话%d" % (name,tel)
+# print(message)
+
+name = "tom"
+tel = 15992478448
+message = "姓名%s 电话%d" % (name,tel)
+print(message)
+
