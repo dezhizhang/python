a210d600ddbe23fa9f21e8f733a02efb63b0a394
dezhizhang <1541609448@qq.com>
2025-07-10 07:26:29 +0800

    feat: while循环

---
+++ lesson18/main.py
@@ -0,0 +1,11 @@
+
+
+height = int(input("请输入您的身高"))
+vip_level = int(int("请输入别"))
+if height > 120:
+    if vip_level >3:
+        print("你获取免费")
+    else:
+        print("您的级别不够")
+else:
+    print("hello world")
---
+++ lesson19/main.py
@@ -0,0 +1,34 @@
+
+# num = 1
+# while num < 100:
+#     print(num)
+#     num+=1
+# print("game over")
+
+# num = 1
+# while num <=100:
+#     print(num)
+#     num += 1
+# print("game over")
+
+# num = 1
+# while num <=100:
+#     print(num)
+#     num+=1
+# print("game over")
+
+# num = 1
+# while num <=100:
+#     print(num)
+#     num+=1
+# print("game over")
+
+num = 100
+sum = 0
+while num > 0:
+    sum += num
+    num = num - 1
+
+
+print(sum)
+
