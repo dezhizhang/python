5a7a7e3ec6f77188113f483bb96977f320c36883
dezhizhang <1541609448@qq.com>
2025-07-12 17:25:30 +0800

    feat: for循环

---
+++ lesson21/main.py
@@ -0,0 +1,20 @@
+
+
+# name = "shuqin.cc"
+#
+# for x in name:
+#     print(x)
+
+# name = "shuqin.cc"
+#
+# for x in name:
+#     print(x)
+
+name = "shuqin.cc"
+num = 0
+for x in name:
+    if x == "c":
+        num+=1
+print(num)
+
+
