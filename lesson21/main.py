8d94daef8f0e0a976dd85a2a26d78dfdf6d7ad0c
dezhizhang <1541609448@qq.com>
2025-07-12 17:51:52 +0800

    feat: for循环

--- lesson21/main.py
+++ lesson21/main.py
@@ -10,11 +10,21 @@
 # for x in name:
 #     print(x)
 
+# name = "shuqin.cc"
+# num = 0
+# for x in name:
+#     print(x)
+#     if x == "c":
+#         num+=1
+# print(num)
+
 name = "shuqin.cc"
 num = 0
 for x in name:
+    print(x)
     if x == "c":
-        num+=1
+        num += 1
+
 print(num)
 
 
---
+++ lesson22/main.py
@@ -0,0 +1,49 @@
+# for x in range(10):
+#     print(x)
+
+# for x in range(10):
+#     print(x)
+#
+
+# for x in range(5,10):
+#     print(x)
+
+# for x in range(10):
+#     print(x)
+
+# for x in range(10):
+#     print(x)
+
+# for x in range(1,100,2):
+#     print(x)
+
+# for x in range(100):
+#     print(x)
+
+# for i in range(100):
+#     print(i)
+#
+# print("hello",i)
+
+# for i in range(100):
+#     print(i)
+#
+# print("hello",i)
+
+# i = 0
+#
+# for i in range(100):
+#     print(i)
+#
+# print(i)
+
+for i in range(100):
+    print(i)
+
+
+
+
+
+
+
+
