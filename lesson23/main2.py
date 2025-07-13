a7588b64e2544476ebcee1d0d686184b0de62b3d
dezhizhang <1541609448@qq.com>
2025-07-13 19:26:20 +0800

    feat:update

---
+++ lesson23/main2.py
@@ -0,0 +1,29 @@
+
+
+# for i in range(1,6):
+#     print("语句1")
+#     continue
+#     print("语句2")
+
+# for i in range(1,6):
+#     print("语句1")
+#     break
+#     print("语句2")
+
+# for i in range(1,6):
+#     print("语句1")
+#     for j in range(1,6):
+#         print("语句2")
+#         break
+#         print("语句3")
+#     print("语句4")
+
+for i in range(1,6):
+    print("语句1")
+    for j in range(1,6):
+        print("语句2")
+        break
+        print("语句3")
+    print("语句4")
+
+
