b0bcfddf146d592325c555f9e782a9ba12bb4e57
dezhizhang <1541609448@qq.com>
2025-07-13 19:59:47 +0800

    feat: function

---
+++ lesson24/main.py
@@ -0,0 +1,16 @@
+
+
+def my_len(data):
+    count = 0
+    for i in data:
+        count += 1
+
+    print(count)
+
+
+str1 = "hello world"
+str2 = "change info my name is "
+
+my_len(str1)
+my_len(str2)
+
