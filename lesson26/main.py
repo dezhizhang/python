686a987b21c9516a51db3092d5397122c7be6fd1
dezhizhang <1541609448@qq.com>
2025-07-14 23:38:05 +0800

    feat: 添加说明文档

---
+++ lesson26/main.py
@@ -0,0 +1,34 @@
+
+
+# def add(a,b):
+#     result = a + b
+#     return result
+#
+# print(add(3,5))
+
+# def add(a,b):
+#     result = a + b
+#     return  result
+#
+# print(add(1,2))
+
+
+# def say_hi():
+#     return None
+#
+# result = say_hi()
+# print(result)
+# print(type(result))
+
+def add(x,y):
+    """
+    add 函数接受两个函数进行2数相加的功能
+    :param x:
+    :param y:
+    :return: 返回值是2数相加的结果
+    """
+
+    result = x + y
+    return  result
+
+print(add(2,5))
