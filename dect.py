6e9764565b78b8fb2884e74c75c22ab352239b12
dezhizhang <1541609448@qq.com>
2025-07-18 23:08:41 +0800

    feat: 调整结构

---
+++ dect.py
@@ -0,0 +1,43 @@
+# my_dect = {"tom":99,"jack":102}
+#
+
+
+# my_dict = {"tom":99,"jack":100,"tom":22}
+# print(my_dict["tom"])
+
+# my_dict = {"tom":99,"jack":100}
+#
+# print(my_dict["tom"])
+
+# my_dict = {"tom":99,"my_set":{"jack":22}}
+#
+# print(my_dict["my_set"]["jack"])
+
+# my_dict = {"tom":99}
+# my_dict["jack"] = 88
+# print(my_dict)
+
+# my_dict = {"tom":88}
+# my_dict["jack"] = 99
+# print(my_dict)
+
+# my_dict = {"tom":100}
+# my_dict["tom"] = 88
+# print(my_dict)
+
+my_dict = {"tom":100}
+my_dict["tom"] = 88
+print(my_dict)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ examples/main.py
@@ -0,0 +1,39 @@
+# my_list = [0,1,2,3,4,5,6]
+# result1 = my_list[1:4]
+# print(f"结果:{result1}")
+
+# my_list = [0,1,2,3,4,5,6]
+# result = my_list[1:4]
+# print(f"结果:{result}")
+
+# my_tuple = (0,1,2,3,4,5)
+# result2 = my_tuple[:]
+# print(f"结果：{result2}")
+
+# my_str = "01234567"
+# result3 = my_str[::-1]
+# print(f"结果4：{result3}")
+
+# my_str = "01234567"
+# result3 = my_str[::-1]
+# print(f"结果：{result3}")
+
+# my_str = "012345678"
+# result = my_str[::-1]
+# print(f"结果:{result}")
+#
+# my_tuple = (0,1,2,3,4,5,6)
+# result2 = my_tuple[::-2]
+# print(f"result2:{result2}")
+
+my_str = "gfedcba"
+
+list = my_str[::-1][2:3]
+print(list)
+
+
+
+
+
+
+
