b581f75a93efa0d8ee3c00bf772964d9aa8f56b6
dezhizhang <1541609448@qq.com>
2025-07-20 19:52:28 +0800

    feat:object

---
+++ examples/object.py
@@ -0,0 +1,15 @@
+
+
+class Student:
+    name = None
+    gender = None
+    age = None
+
+stu = Student()
+stu.name = "tom"
+stu.gender = "男"
+stu.age = 22
+
+print(f"姓名:{stu.name}年龄:{stu.age}性别:{stu.gender}")
+
+
---
+++ examples/object2.py
@@ -0,0 +1,26 @@
+
+
+# class Student:
+#     name = None
+#     age = None
+#     gender = None
+#
+#     def say_hi(self):
+#         print(f"hi 大家好,我是:{self.name}")
+#
+# stu1 = Student()
+# stu1.name = "tom"
+#
+# stu1.say_hi()
+
+class Student:
+    name = None
+    age = None
+    gender = None
+
+    def say_hi(self):
+        print(f"hi 大家好,我是:{self.name}")
+
+stu1 = Student()
+stu1.name = "tom"
+stu1.say_hi()
